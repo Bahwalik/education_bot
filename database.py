7d28b642e82ce38176e1447a04cb5dab48d7c49b
Maxim <maks3651@mail.ru>
2024-11-19 16:26:25 +0300

    /next_question - новая команда, позволяющая отправлять вопросы и проверяющая ответ. database полуавтоматически генерирует таблицу с вопросами. ответы она берет из файла, а вопросы с вариантами ответа нужно подготавливать ручкками. имена даем таким образом - смотрим на цифру последнего вопроса. например, 16, сохраняем фото вопроса в папку question, переименовываем в номер последнего + 1, в нашем случае 17. картинки в png.
    в планах давать перечень вопросов сохранять количество правильных и выдавать молодец человек или нет)

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 import sqlite3
-
+import random
 
 def create_new_user(user_id, username):
     connection = sqlite3.connect('my_database.db')
@@ -16,7 +16,6 @@ def create_new_user(user_id, username):
 
     connection.close()
 
-
 def check_user(user_id):
     connection = sqlite3.connect('my_database.db')
     cursor = connection.cursor()
@@ -25,7 +24,6 @@ def check_user(user_id):
     connection.close()
     return existing_user
 
-
 def add_userinfo(user_id, message):
     connection = sqlite3.connect('my_database.db')
     cursor = connection.cursor()
@@ -33,13 +31,10 @@ def add_userinfo(user_id, message):
     connection.commit()
     connection.close()
 
-
 def process_new_name(message, bot):
     new_name = message.text
     add_userinfo(message.from_user.id, new_name)
     bot.send_message(message.chat.id, 'ФИО изменено')
-
-
 
 def create_user_bd():
     connection = sqlite3.connect('my_database.db')
@@ -52,3 +47,84 @@ def create_user_bd():
     ''')
     connection.commit()
     connection.close()
+
+def create_questions_bd():
+    connection = sqlite3.connect('my_database.db')
+    cursor = connection.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Questions(
+        group_id INTEGER,
+        question_id integer PRIMARY KEY,
+        question TEXT,
+        correct_answer INTEGER
+        )
+    ''')
+    connection.commit()
+    connection.close()
+
+from docx import Document
+
+def parse_and_store_questions(filename, group_id):
+
+    # Подключение к базе данных
+    connection = sqlite3.connect('my_database.db')
+    cursor = connection.cursor()
+
+    # Чтение документа
+    doc = Document(filename)
+    lines = [p.text.strip() for p in doc.paragraphs if p.text.strip()]
+
+    # Переменные для хранения текущих данных
+    current_question = None
+    current_correct_answer = None
+    i = 0
+    while i < len(lines):
+        if '+ans' in lines[i]:
+            i += 1
+            current_correct_answer = lines[i]
+
+            cursor.execute("SELECT MAX(question_id) FROM questions")
+            max_id = cursor.fetchone()[0]
+
+            if max_id is None:
+                next_id = 1
+            else:
+                next_id = max_id + 1
+            current_question = 'question/' + str(next_id)+'.png'
+            cursor.execute('INSERT INTO Questions (group_id, question_id, question, correct_answer) VALUES (?, ?, ?, ?)', (1, next_id, current_question, current_correct_answer))
+            connection.commit()
+        i += 1
+    connection.commit()
+    connection.close()
+
+def give_question(message):
+    connection = sqlite3.connect('my_database.db')
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT question_id FROM questions")
+    question_ids = cursor.fetchall()
+
+    question_ids = [item[0] for item in question_ids]
+
+    random_question_id = random.choice(question_ids)
+
+    connection.commit()
+
+    cursor.execute("SELECT question, correct_answer FROM questions WHERE question_id = ?", (random_question_id,))
+    result = cursor.fetchone()
+
+    question, current_correct_answer = result
+    print(random_question_id, message.chat.id)
+    cursor.execute('UPDATE Users SET question_id = ? WHERE id = ?', (random_question_id, message.chat.id ))
+    connection.commit()
+    connection.close()
+    return question, current_correct_answer
+
+
+def check_answer(message, bot, answer):
+    text = message.text
+
+    if str(answer) != text:
+        bot.send_message(message.chat.id, 'Неверно')
+    else:
+        bot.send_message(message.chat.id, 'Ответ верный!')
--- main.py
+++ main.py
@@ -19,6 +19,20 @@ def handle_rename(message):
     bot.send_message(message.chat.id, "Введите ваше новое ФИО:")
     bot.register_next_step_handler(message, database.process_new_name, bot)  # Передаем bot
 
+@bot.message_handler(commands=['next_question'])
+def handle_rename(message):
+    bot.send_message(message.chat.id, 'Держи вопрос:')
+    question, answer = database.give_question(message)
+    markup = telebot.types.ReplyKeyboardMarkup(row_width=4, one_time_keyboard=True)
+    button1 = telebot.types.KeyboardButton("1")
+    button2 = telebot.types.KeyboardButton("2")
+    button3 = telebot.types.KeyboardButton("3")
+    button4 = telebot.types.KeyboardButton("4")
+
+    markup.add(button1, button2, button3, button4)
+    with open(question, 'rb') as photo:
+        bot.send_photo(message.chat.id, photo,caption='Держи вопрос!',reply_markup=markup)
+    bot.register_next_step_handler(message, database.check_answer, bot, answer)
 
 def register_fio(message):
     user_id = message.from_user.id
